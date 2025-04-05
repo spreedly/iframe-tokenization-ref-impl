f22610fb5a0737aa1b818b26c7b357abf2e3aaff
Esmit Pérez <esmitperez@gmail.com>
2025-04-05 15:18:02 -0700

    Enhance documentation

--- create-cert.sh
+++ create-cert.sh
@@ -26,8 +26,8 @@ openssl genrsa -out private_key.pem 3072
 # Generate a public key from the private key (suppressing output)
 openssl rsa -in private_key.pem -pubout -out public_key.pem > /dev/null 2>&1
 
-# Generate a self-signed certificate, expires in 30 days
-openssl req -new -x509 -days 30 -key private_key.pem -out cert.pem \
+# Generate a self-signed certificate, expires in 1 day
+openssl req -new -x509 -days 1 -key private_key.pem -out cert.pem \
     -subj "/C=US/ST=State/L=City/O=Organization/CN=${DOMAIN_NAME}"
 
 # Create a temporary file for the JSON data
@@ -74,7 +74,7 @@ fi
 nonce=$(uuidgen)
 timestamp=$(date +%s)
 
-# Generate a signature using the private key
+# load your private key so we can use it to generate a signature
 PRIVATE_KEY_RAW=$(cat private_key.pem)
 
 # Only output PRIVATE_KEY_RAW if debug flag is set
@@ -84,32 +84,32 @@ if [ "$DEBUG_PRIVATE_KEY" = "true" ]; then
     echo "DEBUG ─────────────────────────────────────"
 fi
 
-signature=$(echo -n "${nonce}${timestamp}${certificateToken}" | \
+# generate a signature using openssl and base64 encode it with no newlines
+openssl_signature=$(echo -n "${nonce}${timestamp}${certificateToken}" | \
   openssl dgst -sha256 -hmac "$PRIVATE_KEY_RAW" -binary | \
   base64 -w 0)
-
-echo
-echo "╔═══════════════════════════════════════════════════╗"
-echo "║   Example of how to generate the HMAC signature   ║	"
-echo "╚═══════════════════════════════════════════════════╝"
-echo "NONCE: $nonce"
-echo " └─ randomly generated"
-echo "TIMESTAMP: $timestamp"
-echo " └─ current Unix timestamp"
-echo "CERTIFICATE_TOKEN: $certificateToken"
-echo "─────────────────────────────────────"
-echo "SERVER_GENERATED_HMAC: $signature" | tr -d '\n' | cat
-echo
-echo " └─ generated using OpenSSL"
 
 RUBY_CODE=$(cat <<EOF
 require 'openssl'
 require 'base64'
 digest = OpenSSL::Digest.new('sha256')
 hmac_signature = OpenSSL::HMAC.digest(digest, ENV['PRIVATE_KEY'], ENV['SIGNATURE_DATA'])
-puts "SERVER_GENERATED_HMAC: " + Base64.strict_encode64(hmac_signature)
+puts "YOUR_SERVER_GENERATED_SIGNATURE: " + Base64.strict_encode64(hmac_signature)
 EOF
 )
+
+echo
+echo "╔═══════════════════════════════════════════════════╗"
+echo "║   Example of how to generate the HMAC signature   ║	"
+echo "╚═══════════════════════════════════════════════════╝"
+echo "NONCE: $nonce"
+echo " └─ randomly generated UUID"
+echo "TIMESTAMP: $timestamp"
+echo " └─ current Unix timestamp"
+echo "CERTIFICATE_TOKEN: $certificateToken"
+echo "─────────────────────────────────────"
+echo "YOUR_SERVER_GENERATED_SIGNATURE: $openssl_signature"
+echo " └─ generated using OpenSSL"
 echo "─────────────────────────────────────"
 echo $(PRIVATE_KEY=$PRIVATE_KEY_RAW SIGNATURE_DATA=$(echo $nonce$timestamp$certificateToken) ruby -e "$RUBY_CODE")
 echo " └─ generated using Ruby"
